aa60424fc80767614a491f65e787ce4e32c9b0b9
Josh Prewitt <joshua.prewitt@ni.com>
2019-03-25 22:27:19 -0500

    Updated example and added state for installing SL python SDK

--- python/tag/tag example.py
+++ python/tag/tag example.py
@@ -2,10 +2,15 @@ import time
 from systemlink.tagclient import TagClient
 from systemlink.tagclient import messages
 
-update = 3.14
+
+# minion_id is a unique identifier for every system that should be prepended to any tag path to avoid collisions
+file = open("C:\\ProgramData\\National Instruments\\salt\\conf\\minion_id","r")
+minion_id = file.read()
+
+update = 5.14
 
 with TagClient(service_name='TagGenerator') as tag_client:
-    tag_path = "PXI-ATDEMO-2.foo" 
+    tag_path = minion_id + ".foo" 
     tag_type = "DOUBLE" 
     tag_properties = {"nitagRetention":"COUNT"}
     tag_client.create_tag(tag_path, tag_type,tag_properties)
